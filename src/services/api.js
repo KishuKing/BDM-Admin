60d14abb39885b309ee7d13386f142c77d0f6ef5
KishuKing <kn670136@gmail.com>
2026-03-15 23:12:33 +0530

    backend url added to the code

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://my-bmd-backend.onrender.com/api',
 });
 
 // Add a request interceptor to inject token
